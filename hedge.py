53b613b334ed8bfaf807787020d03c8a7f32e684
Janusz Grabis <janusz@frameworks.pl>
2023-07-23 11:58:41 +0200

    netplan configs for master, worker1, worker2

--- hedge.py
+++ hedge.py
@@ -12,10 +12,19 @@ class Hedge:
         class_def = getattr(module, className)
         return class_def(self.repoRootPath) 
 
-    def build(self, params):
+    def build_master(self, params):
+        filehedge = self.createHedgeObject('FileHedge')
+        filehedge.ensureFile('/hedge/master/etc/netplan/50-cloud-init.yaml', '/etc/netplan/50-cloud-init.yaml')
+        
+    def build_worker1(self, params):
+        filehedge = self.createHedgeObject('FileHedge')
+        filehedge.ensureFile('/hedge/worker1/etc/netplan/50-cloud-init.yaml', '/etc/netplan/50-cloud-init.yaml')
+        
+    def build_worker2(self, params):
         logging.debug("You are now in master file with instructions")
         filehedge = self.createHedgeObject('FileHedge')
-        filehedge.ensureFile('/hedge/etc/netplan/50-cloud-init.yaml', '/etc/netplan/50-cloud-init.yaml')
+        filehedge.ensureFile('/hedge/worker2/etc/netplan/50-cloud-init.yaml', '/etc/netplan/50-cloud-init.yaml')
         
+                    
         
         
